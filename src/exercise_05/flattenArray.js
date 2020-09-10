7cd662ebd85e889ed26440962e23155f0e512c75
steven-x-dev <steven-dev@qq.com>
2020-09-11 02:25:38 +0800

    [steven] feat: completed flattenArray

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,20 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) throw new Error('Flatten undefined or null array');
+
+  const res = [];
+
+  for (let i = 0; i < array.length; i += 1) {
+
+    const elem = array[i];
+
+    if (Array.isArray(elem)) {
+        res.push(...elem);
+    } else {
+        res.push(elem);
+    }
+  }
+
+  return res;
 }
