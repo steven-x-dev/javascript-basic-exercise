b53d8f1b14fa2a9045c1d83de839cf702dceaeec
steven-x-dev <steven-dev@qq.com>
2020-09-11 02:31:46 +0800

    [steven] feat: completed arrayToList (not tested, visibility of instance variable value, next of exported class Node externally is unknown)

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -1,3 +1,10 @@
+export class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 export default function arrayToList(array) {
   // This function creates a linked list from an array.
   //
@@ -6,5 +13,16 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) throw new Error('Creating list from undefined array');
+  if (array.length === 0) throw new Error('Creating list from empty array');
+
+  let prev = null;
+
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    const node = new Node(array[i]);
+    node.next = prev;
+    prev = node;
+  }
+
+  return prev;
 }
