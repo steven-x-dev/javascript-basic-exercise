70b4ad2f79f1720391ad9fcb2f65bab68d5cacb6
steven-x-dev <steven-dev@qq.com>
2020-09-11 02:07:28 +0800

    [steven] feat: completed characterCounter

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,16 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === undefined || string === null) return 0;
+
+  if (prediction === undefined) return prediction.length;
+
+  if (prediction === null || prediction.length !== 1) throw new Error('invalid character');
+
+  let count = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (string[i] === prediction) count += 1;
+  }
+
+  return count;
 }
