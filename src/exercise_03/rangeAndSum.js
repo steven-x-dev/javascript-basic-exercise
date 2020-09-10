bfd75e6998101a690441f0a819e53890f0d4ae4f
steven-x-dev <steven-dev@qq.com>
2020-09-11 02:09:20 +0800

    [steven] feat: completed rangeAndSum

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -7,7 +7,15 @@ export function range(start, end) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  const array = [];
+
+  if (end > start) {
+    for (let i = start; i < end; i += 1) array.push(i);
+  } else {
+    for (let i = start; i > end; i -= 1) array.push(i);
+  }
+
+  return array;
 }
 
 export function sum(...numbers) {
@@ -18,5 +26,13 @@ export function sum(...numbers) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (numbers === undefined || numbers === null) return 0;
+
+  let res = 0;
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    res += numbers[i];
+  }
+
+  return res;
 }
